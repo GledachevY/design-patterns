daeb3a32dd790b284e3ad369fa38e4451ef8f86e
Yavor Gledachev <jaking0111@gmail.com>
2021-03-23 11:15:28 +0200

    edit homework

--- src/observer/ObserverMain.java
+++ src/observer/ObserverMain.java
@@ -5,9 +5,9 @@ public class ObserverMain {
 	public static void main(String[] args) {
 			
 		Instructor instructor = new Instructor();
-		Observer obs1 = new Client("Customer 01");
-		Observer obs2 = new Client("Customer 02");
-		Observer obs3 = new Client("Customer 03");
+		Observer obs1 = new Client("Customer 1");
+		Observer obs2 = new Client("Customer 2");
+		Observer obs3 = new Client("Customer 3");
 		
 		instructor.subscribe(obs1);
 		instructor.subscribe(obs2);
